b785f2f221b127c4a1bee8cd6f7725302ac24d57
Victor Martins <victor.martins.duarte@gmail.com>
2022-10-27 22:21:25 -0300

    Chore Req-02 Adds logic to function get_never_ordered_per_customer'

--- src/track_orders.py
+++ src/track_orders.py
@@ -12,19 +12,20 @@ class TrackOrders:
         self._data.append({customer, order, day})
 
     def get_most_ordered_dish_per_customer(self, customer):
-        customers_orders = []
+        customer_orders = []
         for customer, order, _ in self._data:
             if (customer == customer):
-                customers_orders.append(order)
-        return Counter(customers_orders).most_common(1)[0][0]
+                customer_orders.append(order)
+        return Counter(customer_orders).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        all_orders = self.get_all_orders()
-        orders_by_customer = set({})
-        for order in self._data:
-            if order["customer"] == customer:
-                orders_by_customer.add(order["order"])
-        return all_orders.difference(orders_by_customer)
+        all_orders_set = set()
+        orders_by_customer = set()
+        for customers, order, _ in self._data:
+            all_orders_set.add(order)
+            if customers == customer:
+                orders_by_customer.add(order)
+        return all_orders_set - orders_by_customer
 
     def get_days_never_visited_per_customer(self, customer):
         pass
@@ -35,7 +36,7 @@ class TrackOrders:
     def get_least_busy_day(self):
         pass
 
-    def get_all_orders(self):
+    def all_orders_set(self):
         return set(order["order"] for order in self._data)
 
     def orders_by_customer(self, customer):
